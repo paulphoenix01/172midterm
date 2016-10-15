99f04a7671cf4c8b78e247137efb9937deb8c538
Paul <paulphoenix01@gmail.com>
2016-10-15 01:04:15 -0700

    BUY DONE

--- midterm.js
+++ midterm.js
@@ -1,4 +1,4 @@
-var http = require('http');
+//var http = require('http');
 var repl = require('repl');
 var request = require('superagent');
 var __ = require('underscore');
@@ -23,16 +23,17 @@ function eval(cmd, context, file, callback){
 	if(execute==='ORDERS'){return ORDERS();}
 
 	
-	
+	return;	
 
-	callback(null);
+	// callback(null);
 }
 
-function current_exchange(amount, currency){
+function current_exchange(amount, currency, action){
 	console.log("Amount = " + amount);
 	console.log("currentcy = " + currency);
 
 	var result = "";
+	var order = action + " " + amount + " " + currency + " ";
 	// Update new API Json
 	request.get(url)
 	  .set('Accept', 'application/json')
@@ -40,14 +41,20 @@ function current_exchange(amount, currency){
 		var xjson = response.body;
 		//console.log(xjson);
 		if(currency != null){
-		  var btc_per_curr = "btc_to_" + currency; 
-		  var curr_per_btc = currency + "_to_btc";
-		  var rate_btc_curr = find_rate(xjson, btc_per_curr);
-		  var rate_curr_btc = find_rate(xjson, curr_per_btc);
-		  console.log(rate_btc_curr + " " + btc_per_curr);
-		  console.log(rate_curr_btc + " " + curr_per_btc);
+		  var unit_btc_curr = "btc_to_" + currency; 
+		  var unit_curr_btc = currency + "_to_btc";
+		  var rate_btc_curr = find_rate(xjson, unit_btc_curr);
+		  var rate_curr_btc = find_rate(xjson, unit_curr_btc);
+		  console.log(rate_btc_curr + " " + unit_btc_curr);
+		  console.log(rate_curr_btc + " " + unit_curr_btc);
+			
+		  var display = "Order to " + order + " worth of BTC queued @ "
+		+ rate_btc_curr + " " + "BTC/" + currency.toUpperCase() + " ("
+		+ rate_curr_btc + " BTC)"; 
+		
+		  console.log(display);
 		}});
- 	
+ 			
 	return result;
 
 }
@@ -62,9 +69,18 @@ var find_rate = function(json, unit){
 var BUY = function(args){
 	console.log("BUY function");
 	if(args[2] != null){	
-	  current_exchange(args[1], args[2].trim().toLowerCase());
+	  current_exchange(args[1].trim(), args[2].trim().toLowerCase(), "BUY");
  	}
+	else if(args[2] == null){
+	  if(args[1] > 0){
+		var display = "Order to BUY " + args[1].trim() + " BTC queued";
+	  	console.log(display);
+	  }else{
+	  	console.log("Invalid or No Amount specified. Please try again!");
+	  }
+	}
 	
+	return;		
 }
 
 var SELL = function(){
