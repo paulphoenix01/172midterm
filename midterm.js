3f6fb598a7061416203d579e78258444c14eb092
Paul <paulphoenix01@gmail.com>
2016-10-15 00:34:34 -0700

    converting done

--- midterm.js
+++ midterm.js
@@ -31,33 +31,40 @@ function eval(cmd, context, file, callback){
 function current_exchange(amount, currency){
 	console.log("Amount = " + amount);
 	console.log("currentcy = " + currency);
-	var unit = "btc_to_usd";
+
 	var result = "";
 	// Update new API Json
- 	var xjson = 
 	request.get(url)
 	  .set('Accept', 'application/json')
           .end(function(error, response){
 		var xjson = response.body;
 		//console.log(xjson);
 		if(currency != null){
-		  unit = currency.trim().toLowerCase() + "_to_btc";
-		  console.log(find_rate(xjson, unit));
+		  var btc_per_curr = "btc_to_" + currency; 
+		  var curr_per_btc = currency + "_to_btc";
+		  var rate_btc_curr = find_rate(xjson, btc_per_curr);
+		  var rate_curr_btc = find_rate(xjson, curr_per_btc);
+		  console.log(rate_btc_curr + " " + btc_per_curr);
+		  console.log(rate_curr_btc + " " + curr_per_btc);
 		}});
- 
+ 	
 	return result;
 
 }
 
 var find_rate = function(json, unit){
 	//return __.filter(json, function(data){ return data.unit;}); 
-	console.log(unit);	
-	console.log(json[unit]);
+	//console.log(unit);	
+	//console.log(json[unit]);
+	return json[unit];
 }
 
-var BUY = function(parameter){
+var BUY = function(args){
 	console.log("BUY function");
-	current_exchange(parameter[1], parameter[2]);	
+	if(args[2] != null){	
+	  current_exchange(args[1], args[2].trim().toLowerCase());
+ 	}
+	
 }
 
 var SELL = function(){
